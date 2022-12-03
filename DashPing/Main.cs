4890ef40e0a3f18e7cbb593773eaf616c50179ac
propstg <g@prop.st>
2022-12-03 05:18:27 -0500

    refactor

--- DashPing/Main.cs
+++ DashPing/Main.cs
@@ -1,41 +1,56 @@
-﻿using BepInEx;
-using Kitchen;
-using System.Collections;
+﻿using Kitchen;
+using KitchenMods;
 using System.Linq;
 using UnityEngine;
 
-namespace DashPing {
+namespace KitchenDashPing {
 
-    [BepInProcess("PlateUp.exe")]
-    [BepInPlugin(MOD_ID, MOD_NAME, MOD_VERSION)]
-    public class Mod : BaseUnityPlugin {
+    public class Main : Mod {
+        public const string MOD_NAME = "dashping";
+        public const string MOD_VERSION = "0.2.0";
 
-        public const string MOD_ID = "dashping";
-        public const string MOD_NAME = "Dash Ping";
-        public const string MOD_VERSION = "0.1.2";
+        public Main() : base(MOD_NAME) {
+            Debug.LogWarning($"{MOD_NAME}: Loaded");
+        }
+    }
+
+    public class DashSystem : GenericSystemBase, IModSystem {
 
         private const float INITIAL_SPEED = 3000f;
         private const float DASH_SPEED = 6000f;
         private const float DASH_DURATION = 0.35f;
+        private bool isDashing = false;
+        private float dashCooldown;
 
-        public void Awake() {
-            Debug.LogWarning($"BepInEx mod loaded: {MOD_NAME} {MOD_VERSION}");
-        }
+        protected override void OnUpdate() {
+            if (dashCooldown > 0) {
+                dashCooldown -= Time.DeltaTime;
 
-        public void Update() {
+                if (isDashing && dashCooldown <= 0) {
+                    isDashing = false;
+                    returnSpeedToNormal();
+                }
+            }
+
             if (isPingDown()) {
-                PlayerView[] players = PlayerInfoManager.FindObjectsOfType<PlayerView>();
-                players.ToList().ForEach(setSpeedToDash);
-                StartCoroutine(returnSpeedToNormal(players));
+                if (isDashNoLongerOnCoolDown()) {
+                    isDashing = true;
+                    dashCooldown = DASH_DURATION;
+
+                    PlayerView[] players = PlayerInfoManager.FindObjectsOfType<PlayerView>();
+                    players.ToList().ForEach(setSpeedToDash);
+                }
             }
         }
 
         private bool isPingDown() => isKeyDown(KeyCode.Joystick1Button1) || isKeyDown(KeyCode.L);
 
-        private bool isKeyDown(KeyCode key) => UnityInput.Current.GetKeyDown(key);
+        private bool isKeyDown(KeyCode key) => Input.GetKeyDown(key);
 
-        private IEnumerator returnSpeedToNormal(PlayerView[] players) {
-            yield return new WaitForSeconds(DASH_DURATION);
+        private bool isDashNoLongerOnCoolDown() => dashCooldown <= 0;
+
+        private void returnSpeedToNormal() {
+            PlayerView[] players = PlayerInfoManager.FindObjectsOfType<PlayerView>();
             players.ToList().ForEach(setSpeedToNormal);
         }
 
