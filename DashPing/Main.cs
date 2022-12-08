50ffbe2fc29578a96cfde3e05c4bb2d0a145c0a9
propstg <g@prop.st>
2022-12-08 02:29:50 -0500

    workaround for clients

--- DashPing/Main.cs
+++ DashPing/Main.cs
@@ -1,26 +1,32 @@
 ﻿using Kitchen;
-using KitchenMods;
+using KitchenLib;
 using System.Linq;
+using System.Reflection;
 using UnityEngine;
 
 namespace KitchenDashPing {
 
-    public class DashSystem : GenericSystemBase, IModSystem {
+    public class DashSystem : BaseMod {
+
+        public const string MOD_ID = "blargle.DashPing";
+        public const string MOD_NAME = "Dash Ping";
+        public const string MOD_VERSION = "0.1.4";
 
         private const float INITIAL_SPEED = 3000f;
         private const float DASH_SPEED = 6000f;
         private const float DASH_DURATION = 0.35f;
+        private const float DASH_REDUCE_PER_UPDATE = 0.03125f;
+
         private bool isDashing = false;
         private float dashCooldown;
 
-        protected override void Initialise() {
-            base.Initialise();
-            Debug.Log($"DashPing: loaded.");
+        public DashSystem() : base(MOD_NAME, MOD_VERSION, "1.1.2", Assembly.GetExecutingAssembly()) {
+            Debug.Log($"Mod loaded: {MOD_ID} {MOD_VERSION}");
         }
 
         protected override void OnUpdate() {
             if (dashCooldown > 0) {
-                dashCooldown -= Time.DeltaTime;
+                dashCooldown -= DASH_REDUCE_PER_UPDATE;
 
                 if (isDashing && dashCooldown <= 0) {
                     isDashing = false;
